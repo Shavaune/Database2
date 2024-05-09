4b19fd171ca4966ba8ff1803b89d27a4c14f0dbb
Shavaune <114256220+Shavaune@users.noreply.github.com>
2024-05-09 04:51:16 -0500

    .

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about container">
     <h2>About</h2>
-    <p>This Project is stressing me out.</p>
+    <p>This Project is stressing me out X2.</p>
   </div>
 </template>
 
